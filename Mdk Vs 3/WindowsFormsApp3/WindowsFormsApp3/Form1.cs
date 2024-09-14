9bfc7fb1cf2cf37f582fe668d87471875165e5b6
Тимур <gitmdk17@gmail.com>
2024-09-14 11:23:47 +0400

    Add files via upload

---
+++ Mdk Vs 3/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1) 
+            {
+               pictureBox2.Hide();
+               pictureBox1.Show();
+            }
+            
+            else
+            {
+                pictureBox2.Show();
+                pictureBox1.Hide();
+            }
+        }
+
+    }
+}
