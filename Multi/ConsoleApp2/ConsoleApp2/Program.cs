eb85539f7cdbefa69725f1a42f8b0d9385f9a37b
Тимур <gitmdk17@gmail.com>
2024-09-15 21:44:32 +0300

    Add files via upload

---
+++ Multi/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                double firstvalue, secondvalue, result;
+
+
+                try
+                {
+                    Console.WriteLine("Введите первое число:");
+                    firstvalue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число:");
+                    secondvalue = double.Parse(Console.ReadLine());
+                    result = firstvalue * secondvalue;
+                    Console.WriteLine("Результат:" + result);
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка! Введите число");
+                }
+
+
+
+
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
