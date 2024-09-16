d399f8f46dfbb8df506a4bb7bb961a95a1ad588c
Тимур <gitmdk17@gmail.com>
2024-09-16 10:40:35 +0400

    Add files via upload

---
+++ Vs ( all )/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Clear();
+
+                
+
+                double firstValue, secondValue;
+                string Action;
+
+
+
+                Console.WriteLine("Введите первое число:");
+                firstValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                secondValue= double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+                Action = Console.ReadLine();
+
+                if (Action == "+")
+
+                {
+                    Console.WriteLine(firstValue + secondValue);
+                }
+
+                else if ( Action == "-")
+                {
+                    Console.WriteLine(firstValue - secondValue);
+                }
+
+                else if ( Action == "*")
+                {
+                    Console.WriteLine(firstValue * secondValue);
+                }
+
+                else if ( Action == "/")
+                {
+                    if (secondValue == 0)
+                        Console.WriteLine(0);
+                    else
+                        Console.WriteLine(firstValue / secondValue);
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Ошибка! Неизвестное действие");
+                }
+
+
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
